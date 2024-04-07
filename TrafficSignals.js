4deffdb4c573a1cb4da53f3536edc0be84f11d65
Muhammad Yasir Asif <4459yasirasif@gmail.com>
2024-04-07 15:41:18 +0500

    Add files via upload

---
+++ TrafficSignals.js
@@ -0,0 +1,15 @@
+let Btn1=document.getElementById("btn1");
+let Btn2=document.getElementById("btn2");
+let Btn3=document.getElementById("btn3");
+let Btn4=document.getElementById("btn4");
+function ready() {
+Yellow.style.display="block";
+}
+function go() {
+Green.style.display="block";
+}
+function stop() {
+Red.style.display="block";
+}
+
+
